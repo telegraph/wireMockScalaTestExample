4d17018367e90b978ff6e3adfcdc6a847e3bfa48
Parshotam Toora <toorap@TMGM-C02T62PLFVH4.local>
2017-08-08 10:13:59 +0100

    add coverage

--- src/main/scala/com/testing/mock/WireMockServer.scala
+++ src/main/scala/com/testing/mock/WireMockServer.scala
@@ -1,10 +1,13 @@
 package com.testing.mock
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import com.github.tomakehurst.wiremock.client.WireMock._
+import com.github.tomakehurst.wiremock.common.FileSource
 import com.github.tomakehurst.wiremock.core.Admin
-import com.github.tomakehurst.wiremock.extension.{Parameters, PostServeAction}
+import com.github.tomakehurst.wiremock.extension.{Parameters, PostServeAction, ResponseTransformer}
+import com.github.tomakehurst.wiremock.http.{Request, Response, ResponseDefinition}
 import com.github.tomakehurst.wiremock.stubbing.ServeEvent
 
 import scala.collection.mutable.ListBuffer
@@ -20,12 +23,10 @@ object WireMockServer {
   var wireMockServer: WireMockServer = null;
 
   def start = {
-    assert(stateCache.errorsList.length==0, stateCache.errorsList.reverse.last)
     wireMockServer.start()
   }
 
   def stop = {
-    assert(stateCache.errorsList.length==0, stateCache.errorsList.reverse.last)
     wireMockServer.stop()
   }
 
@@ -36,7 +37,7 @@ object WireMockServer {
 
     println ( s"running on port $port")
 
-    wireMockServer = new WireMockServer(options().port(port).extensions(new ValidateStateModel()))
+    wireMockServer = new WireMockServer(options().port(port).extensions(new ValidateStateTransitionTransformer()))
   }
 
   def setUpMockResponses(filePath:String) = {
@@ -46,25 +47,25 @@ object WireMockServer {
       jsonPath=filePath
 
     wireMockServer.stubFor(get(urlMatching(".*/happy$"))
-      .withPostServeAction("validateState", new ParameterTemplate)
       .willReturn(
         aResponse()
+          .withTransformerParameter("action", "happy")
           .withHeader("Content-Type", "application/json")
           .withBody(Source.fromFile(s"$jsonPath/happy.json").mkString)
           .withStatus(200)))
 
     wireMockServer.stubFor(get(urlMatching(".*/delete$"))
-      .withPostServeAction("validateState", new ParameterTemplate)
       .willReturn(
         aResponse()
+          .withTransformerParameter("action", "delete")
           .withHeader("Content-Type", "application/json")
           .withBody("Successfully deleted")
           .withStatus(200)))
 
     wireMockServer.stubFor(get(urlMatching(".*/put$"))
-      .withPostServeAction("validateState", new ParameterTemplate)
       .willReturn(
         aResponse()
+          .withTransformerParameter("action", "put")
           .withHeader("Content-Type", "application/json")
           .withBody("Successfully added")
           .withStatus(201)))
@@ -88,32 +89,49 @@ object WireMockServer {
           .withStatus(200)))
   }
 
-  object stateCache {
+
+  object ValidateStateModel {
+
     var count = 0
     var errorsList = ListBuffer[String]()
-  }
 
-  class ValidateStateModel extends PostServeAction {
-
-    override def getName: String = "validateState"
-
-    override def doAction(serveEvent: ServeEvent, admin: Admin, parameters: Parameters) = {
-
-      val action = serveEvent.getRequest.getUrl.split("/").last
+    def isValidStateTransition(action: String) : Boolean = {
 
       if (action == "get") {
         // ok
       } else if (List("put", "post").contains(action)) {
-        stateCache.count = stateCache.count + 1
-      } else if (action == "delete" && stateCache.count==0) {
-        stateCache.errorsList += "Error in call:" + serveEvent.getRequest.getAbsoluteUrl + " - Cannot delete from empty state ... doh!"
-      } else if (action=="delete" && stateCache.count>0) {
-        stateCache.count = stateCache.count -1
+        count = count + 1
+      } else if (action == "delete" && count==0) {
+        errorsList += "Error: - Cannot delete from empty state ... doh!"
+        return false
+      } else if (action=="delete" && count>0) {
+        count = count -1
       }
+      true
     }
   }
 
-  class ParameterTemplate extends Serializable {}
+
+  class ValidateStateTransitionTransformer extends ResponseTransformer {
+
+    override def transform(request :Request, response: Response, files :FileSource, parameters: Parameters) : Response = {
+
+      if (ValidateStateModel.isValidStateTransition(parameters.getString("action"))==false) {
+
+        Response.Builder.like(response)
+          .but()
+          .body("Invalid state transition")
+          .status(500)
+          .build();
+      } else {
+        Response.Builder.like(response).build()
+      }
+    }
+
+    override def getName() : String = {
+      "validateStateTransition"
+    }
+  }
 
 }
 
